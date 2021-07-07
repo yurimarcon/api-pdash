f9e1509a5905cd6588df7b60457f990fb828d930
Yuri Marcon <yuri.erik.oliveira@gmail.com>
2021-07-07 17:28:00 -0300

    estilização da barra de scroll

--- src/App.vue
+++ src/App.vue
@@ -94,12 +94,29 @@ import ButtonPluss from './components/buttonsNavRight/ButtonPluss.vue'
 .Altura{
   min-height: 90vh;
 }
+.Recize{
+  resize: horizontal;
+  overflow: auto;
+}
+
 .scroll-container{
   overflow-y: scroll;
   height:90vh;
 }
-.Recize{
-  resize: horizontal;
-  overflow: auto;
+::-webkit-scrollbar {
+	width: 10px;
+	height: 10px;
+}
+::-webkit-scrollbar-button:start:decrement,
+::-webkit-scrollbar-button:end:increment  {
+	display: none;
+}
+::-webkit-scrollbar-track-piece  {
+	background-color: #474747;
+	-webkit-border-radius: 3px;
+}
+::-webkit-scrollbar-thumb:vertical {
+	background-color: rgb(116, 116, 116);
+	-webkit-border-radius: 3px;
 }
 </style>
